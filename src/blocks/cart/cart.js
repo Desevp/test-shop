0ca27e18c2b5f964c4a4a544e23d8736111ca46c
Jane Des <desevp@gmail.com>
2018-01-20 12:41:03 +0300

    Add cart delete, price and validation

--- src/blocks/cart/cart.js
+++ src/blocks/cart/cart.js
@@ -1,13 +1,14 @@
-// document.addEventListener('DOMContentLoaded', function(){});
-// (function(){
-// код
-// }());
 
 var cart = function() {
   var shoppingCart = {};
 
   function init() {
     changeCart($('.js-cart-product'));
+    initDeleteProduct($('.js-cart-product-delete'));
+
+    $('#js-delivery input').change(function() {
+      updateTotalPrice();
+    });
   }
 
   function addToCart(idItem, count) {
@@ -21,56 +22,107 @@ var cart = function() {
 
     addToLocalStorage(shoppingCart);
   }
-    function addToLocalStorage(obj) {
-      localStorage.setItem('shoppingCart', JSON.stringify(obj));
-    }
 
-    function getLocalStorage(name) {
-      if (localStorage.getItem(name)) {
-        return JSON.parse(localStorage.getItem(name));
+  function addToLocalStorage(obj) {
+    localStorage.setItem('shoppingCart', JSON.stringify(obj));
+  }
+
+  function changeCart(elements) {
+
+    elements.find('.js-counter-value').change(function() {
+      updateCountProduct(
+        $(this).closest('.js-cart-product').data('id'),
+        $(this).val()
+      );
+      updateProductPrice($(this).closest('.js-cart-product'));
+    });
+  }
+
+  function updateCountProduct(idItem, count) {
+    if (shoppingCart[idItem]) {
+      shoppingCart[idItem] = count;
+      addToLocalStorage(shoppingCart);
+    }
+  }
+
+  function updateProductPrice(product) {
+    var productCounts = product.find('.js-counter-value').val(),
+        totalProductPrice = product.find('.js-cart-product-total'),
+        productPrice = product.find('.js-cart-product-price').data('price'),
+        productSale = product.find('.js-cart-product-sale').data('sale');
+
+    totalProductPrice.text(parseInt(productCounts) * productPrice * (1 - productSale / 100));
+    updateTotalPrice();
+  }
+
+  function initDeleteProduct(btn) {
+    btn.on('click', function() {
+      aa= $(this).closest('.js-cart-product').data('id');
+      delFromCart($(this).closest('.js-cart-product').data('id'));
+    });
+  }
+
+  function delFromCart(id) {
+    if (shoppingCart[id]) {
+      var product = $('.js-cart-product').filter('[data-id="' + id + '"]');
+      var timeoutDel = 300;
+
+
+      delete shoppingCart[id];
+      addToLocalStorage(shoppingCart);
+
+      if (jQuery.isEmptyObject(shoppingCart)) {
+        $('.js-cart').fadeOut(timeoutDel);
+        setTimeout(function(){
+          $('.js-cart').empty();
+          $('.js-cart').wrapInner('<div class="cart__empty">В корзине пусто</div>');
+          $('.js-cart').fadeIn(timeoutDel);
+        }, timeoutDel);
+      }
+      else {
+        product.slideUp(timeoutDel);
+        setTimeout(function(){
+          product.remove();
+          updateTotalPrice();
+        }, timeoutDel);
       }
     }
+  }
 
-    function changeCart(elements) {
+  function getShipiningValue() {
+    var minPrice = parseInt($('#js-delivery').data('delivery-max-price')),
+        price = getTotalPrice(),
+        courierTextEl = $('.js-delivery-courier').siblings('label').find('span');
 
-      elements.find('.js-counter-value').change(function() {
-        updateCountProduct(
-          $(this).closest('.js-cart-product').data('id'),
-          $(this).val()
-        );
-      });
+    if (price >= minPrice) {
+      $('.js-delivery-courier').val('0');
+      courierTextEl.html('0');
     }
+    else {
+      var deliveryPriceEl = $('.js-delivery-courier'),
+          deliveryPriceValue = deliveryPriceEl.data('delivery-price');
 
-    function updateCountProduct(idItem, count) {
-      console.log('counter');
-      if (shoppingCart[idItem]) {
-        shoppingCart[idItem] = count;
-        addToLocalStorage(shoppingCart);
-      }
+      deliveryPriceEl.val(deliveryPriceValue);
+      courierTextEl.html(deliveryPriceValue);
     }
+    return parseInt($('#js-delivery input').filter(':checked').val());
+  }
 
+  function getTotalPrice() {
+    totalPrice = 0;
+    $('.js-cart-product-total').each(function(){
+      totalPrice = totalPrice + parseInt($(this).text());
+    });
+    return totalPrice;
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-  console.log('LocalStorage');
-
-  console.log(getLocalStorage('shoppingCart'));
-
-
-
+  function updateTotalPrice() {
+    $('.js-subtotal-price').text(new Intl.NumberFormat('ru-RU').format(getTotalPrice()));
+    var price = getShipiningValue() + getTotalPrice();
+    $('.js-total-price').text(price);
+  }
 
   return {
-    // init: init,
     addToCart: function(idItem, itemCount) {
       addToCart(idItem, itemCount);
     },
@@ -80,7 +132,7 @@ var cart = function() {
   }
 }();
 
-cart.addToCart('id1', 1);
-cart.addToCart('id2', 5);
+cart.addToCart('1', 1);
+cart.addToCart('2', 1);
 
 cart.init();
--- src/blocks/sale-form/sale-form.js
+++ src/blocks/sale-form/sale-form.js
@@ -1,4 +1,49 @@
-// document.addEventListener('DOMContentLoaded', function(){});
-// (function(){
-// код
-// }());
+(function(){
+  var saleForm = $('#js-sale-form'),
+      saleFormSubmit = $('.js-sale-form-btn');
+
+  saleFormParams = {
+    errorClass: 'field-input_error',
+    rules: {
+      'saleСoupon': {
+        required: true
+      }
+    },
+    messages: {
+      'saleСoupon': {
+        required: 'Введите купон'
+      }
+    },
+    errorPlacement: function(error, element) {
+      var currentName = element.attr('name');
+      var errorMessage = saleFormValidate.settings.messages[currentName].required;
+
+      element.attr('placeholder', errorMessage);
+    }
+  }
+
+  var saleFormValidate = saleForm.validate(saleFormParams);
+
+  saleFormSubmit.click(function(){
+    if (saleForm.valid()) {
+      $.ajax({
+          url: '',
+          type: 'post',
+          data: saleForm.serialize(),
+          success: function(data, textStatus, jqXHR) {
+            var message;
+            // TODO: add сheck сoupon
+            if (!data) {
+              message = 'Coupon is not valid';
+            }
+            else {
+              message = 'Coupon is valid';
+            }
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+              saleFormSubmit.text('Попробуйте еще раз');
+          }
+      });
+    }
+  });
+}());
